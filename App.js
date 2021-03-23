ab6928afd0c2b8ce8813c14ed3effb4d2205b527
Arunagirinathan Rajan <arunagirinathan.rajan@freshworks.com>
2021-03-23 20:43:31 +0530

    :sparkles: add bottomsheet for new transaction

--- App.js
+++ App.js
@@ -1,29 +1,42 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, FlatList } from "react-native";
 import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  TouchableOpacity,
-  FlatList,
-} from "react-native";
+  Header,
+  Button,
+  Icon,
+  BottomSheet,
+  ListItem,
+} from "react-native-elements";
+
+import data from "./resource/TransactionData";
 
 export default function App() {
   const [name, setName] = useState("");
-  const [amount, setAmount] = useState(0);
-  const [transactions, setTransactions] = useState([
+  const [amount, setAmount] = useState("");
+  const [transactions, setTransactions] = useState(data);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const list = [
     {
-      id: 1,
-      name: "salary",
-      amount: 4000,
+      title: "Add",
+      containerStyle: { backgroundColor: "green" },
+      titleStyle: { color: "white" },
+      onPress: () => addTransaction(),
     },
     {
-      id: 2,
-      name: "tax",
-      amount: -400,
+      title: "Cancel",
+      containerStyle: { backgroundColor: "red" },
+      titleStyle: { color: "white" },
+      onPress: () => closeBottomSheet(),
     },
-  ]);
+  ];
+
+  const closeBottomSheet = () => {
+    setIsVisible(false);
+    setName("");
+    setAmount("");
+  };
 
   const addTransaction = () => {
     setTransactions((prev) => {
@@ -36,8 +49,7 @@ export default function App() {
         ...prev,
       ];
     });
-    setName("");
-    setAmount(0);
+    closeBottomSheet();
   };
 
   const getTotal = () => {
@@ -59,45 +71,82 @@ export default function App() {
   };
 
   const renderTransaction = ({ item }) => {
+    const isExpense = item.amount < 0;
     return (
       <View style={[styles.tracker, styles.item]}>
-        <Text style={[styles.child]}>{item.name}</Text>
-        <Text style={[styles.child]}>{item.amount.toString()}</Text>
+        <Text style={[styles.childText]}>{item.name}</Text>
+        <Text
+          style={[
+            styles.ChildAmount,
+            isExpense ? styles.cExpense : styles.cIncome,
+          ]}
+        >
+          $ {item.amount.toString()}
+        </Text>
       </View>
     );
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Expense Tracker</Text>
-      <Text style={[styles.title, styles.total]}>{getTotal()}</Text>
+      <Header
+        placement="left"
+        leftComponent={{ icon: "attach-money", ...styles.left }}
+        centerComponent={{
+          text: "Expense Tracker",
+          style: styles.center,
+        }}
+        containerStyle={styles.header}
+      />
+      <Text style={[styles.title, styles.total]}>$ {getTotal()}</Text>
       <View style={styles.tracker}>
-        <Text style={[styles.label, styles.income]}>{getIncome()}</Text>
-        <Text style={[styles.label, styles.expense]}>{getExpense()}</Text>
+        <Text style={[styles.label, styles.income]}>$ {getIncome()}</Text>
+        <Text style={[styles.label, styles.expense]}>
+          $ {Math.abs(getExpense())}
+        </Text>
       </View>
-      <Text style={styles.title}>New Transaction</Text>
-      <TextInput
-        style={[styles.input, styles.text]}
-        onChangeText={setName}
-        value={name}
-        placeholder="Enter Transaction Name"
+      <BottomSheet
+        isVisible={isVisible}
+        containerStyle={{ backgroundColor: "rgba(0.5, 0.25, 0, 0.2)" }}
+      >
+        <TextInput
+          style={[styles.input, styles.text]}
+          onChangeText={setName}
+          value={name}
+          placeholder="Enter Transaction Name"
+        />
+        <TextInput
+          style={[styles.input, styles.text]}
+          onChangeText={setAmount}
+          value={amount.toString()}
+          placeholder="Enter Amount"
+          keyboardType="numeric"
+        />
+        {list.map((l, i) => (
+          <ListItem
+            key={i}
+            containerStyle={l.containerStyle}
+            onPress={l.onPress}
+          >
+            <ListItem.Content>
+              <ListItem.Title style={l.titleStyle}>{l.title}</ListItem.Title>
+            </ListItem.Content>
+          </ListItem>
+        ))}
+      </BottomSheet>
+      <Button
+        icon={<Icon name="add" size={30} color="white" />}
+        title="Add transaction"
+        style={styles.button}
+        onPress={() => setIsVisible(true)}
       />
-      <TextInput
-        style={[styles.input, styles.text]}
-        onChangeText={setAmount}
-        value={amount.toString()}
-        placeholder="Enter Amount"
-        keyboardType="numeric"
-      />
-      <TouchableOpacity style={styles.button} onPress={addTransaction}>
-        <Text style={styles.text}>Add Transaction</Text>
-      </TouchableOpacity>
-      <Text style={styles.title}>Recent Transactions</Text>
+      <Text style={[styles.title, { textAlign: "left", paddingLeft: 5 }]}>
+        Recent Transactions
+      </Text>
       <FlatList
         data={transactions}
         renderItem={renderTransaction}
-        keyExtractor={(item) => item.id}
-        style={styles.list}
+        keyExtractor={(item) => item.id.toString()}
       />
       <StatusBar style="auto" />
     </View>
@@ -107,16 +156,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 80,
-    backgroundColor: "#fff",
+  },
+  header: {
+    height: 90,
+  },
+  left: {
+    paddingTop: 5,
+    color: "white",
+  },
+  center: {
+    fontSize: 20,
+    paddingTop: 5,
+    color: "white",
+    fontWeight: "bold",
   },
   tracker: {
     flexDirection: "row",
   },
   button: {
     alignItems: "center",
-    backgroundColor: "#DDDDDD",
-    padding: 10,
   },
   box: {
     width: 50,
@@ -147,8 +205,6 @@ const styles = StyleSheet.create({
     minWidth: "48%",
     textAlign: "center",
     color: "white",
-    textAlign: "center",
-    marginBottom: 10,
     fontSize: 24,
   },
   income: {
@@ -158,10 +214,27 @@ const styles = StyleSheet.create({
     backgroundColor: "red",
   },
   item: {
-    alignContent: "space-around",
+    padding: 15,
+    backgroundColor: "#f8f8f8",
+    borderBottomWidth: 1,
+    borderColor: "#eee",
+    justifyContent: "space-between",
   },
-  child: {
+  childText: {
+    textAlign: "center",
+    fontSize: 23,
+  },
+  ChildAmount: {
     textAlign: "center",
     fontSize: 20,
+  },
+  input: {
+    backgroundColor: "white",
+  },
+  cIncome: {
+    color: "green",
+  },
+  cExpense: {
+    color: "red",
   },
 });
